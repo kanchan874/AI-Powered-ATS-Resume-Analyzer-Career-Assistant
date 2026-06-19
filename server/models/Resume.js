ec1894ecab98aca27eef17f98efcc61b0f0467f8
kanchan874 <kanchangaikwad2006@gmail.com>
2026-06-19 23:03:22 +0530

    Add server/models/Resume.js

---
+++ server/models/Resume.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const resumeSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  resumeUrl: {
+    type: String, // Optional for Phase 1 MVP, can be added later
+  },
+  parsedData: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Resume', resumeSchema);
